7222887c27828dd536421a24edb7a2cd741d2408
Duda <dudasouza@live.com>
2023-10-05 06:24:13 -0300

    .

--- GetLang/CriarMusica.xaml.cs
+++ GetLang/CriarMusica.xaml.cs
@@ -1,9 +1,54 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.Maui.Animations;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
 namespace GetLang;
 
 public partial class CriarMusica : ContentPage
 {
-	public CriarMusica()
+    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    public CriarMusica()
 	{
 		InitializeComponent();
 	}
+
+
+private void Button_Clicked(object sender, EventArgs e)
+{
+    using (var connection = new SqliteConnection("Data Source=hello.db"))
+    {
+
+
+            string nome = nomeMusica.Text;
+            string letra = letraMusica.Text;
+
+
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO listaMusica (nome, letra) VALUES (@nome, @letra)";
+
+        command.Parameters.AddWithValue("@nome", nome);
+        command.Parameters.AddWithValue("@letra", letra);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+            string text = "Musica Adicionada!";
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 14;
+
+            var toast = Toast.Make(text, duration, fontSize);
+            toast.Show(cancellationTokenSource.Token);
+
+            nomeMusica.Text = string.Empty;
+            letraMusica.Text = string.Empty;
+    }
+}
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        nomeMusica.Text = string.Empty; 
+        letraMusica.Text = string.Empty;
+    }
 }
--- GetLang/MainPage.xaml.cs
+++ GetLang/MainPage.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace GetLang
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Maui.Animations;
+using Microsoft.Maui.Devices.Sensors;
+using System;
+using System.Diagnostics;
+
+namespace GetLang
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
+
+            Conectar();
+
         }
 
         async void Btn_add(object sender, EventArgs e)
@@ -13,5 +21,46 @@
             await Navigation.PushAsync(new CriarMusica());
         }
 
+
+        public void Conectar()
+        {
+            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            {
+                connection.Open();
+                Debug.WriteLine("Conectado");
+
+                var command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS listaMusica (id INTEGER PRIMARY KEY AUTOINCREMENT, nome STRING, letra STRING)";
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+      
+        public void ImprimirDatos()
+        {
+            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM listaMusicas";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0); // Índice 0 corresponde a la columna "id"
+                        string nome = reader.GetString(1); // Índice 1 corresponde a la columna "name"
+                        string letra = reader.GetString(2); // Índice 2 corresponde a la columna "letra"
+
+                        Debug.WriteLine($"ID: {id}, Nombre: {nome}, Letra: {letra}");
+                    }
+                }
+
+                connection.Close();
+            }
+        }
     }
 }
